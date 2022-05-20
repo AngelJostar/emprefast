d32a0866610a3b13649f87eb4f09ba2d27457c01
AngelJostar <angelrojas@ciencias.unam.mx>
2022-05-20 10:50:07 -0500

    instalar storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
-
 }
+
+
+const box = document.getElementById('box');
+  box.style.backgroundColor = 'lime';
+  box.style.color = 'red';
+  box.style.fontSize = '2em';
+
